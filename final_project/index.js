658d6744ef700fb94482f641335e0c631dc97e1a
Matvey Bragin <veron.vbn1234@gmail.com>
2023-03-20 09:11:15 -0400

    First save to repository

--- final_project/index.js
+++ final_project/index.js
@@ -8,12 +8,46 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer",session({
+    secret:"fingerprint_customer",
+    resave: true,
+    saveUninitialized: true
+}));
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+
+    if(req.session.authorization) {
+        let token = req.session.authorization['accessToken']; // Access Token
+        
+        jwt.verify(token, "access",(err,user)=>{
+            if(!err){
+                req.user = user;
+                next();
+            }else{
+                return res.status(403).json({message: "User not authenticated"})
+            }
+         });
+     } else {
+         return res.status(403).json({message: "User not logged in"})
+     }
 });
- 
+
+app.post("/login", (req,res) => {
+    const user = req.body.user;
+    //console.log(req);
+    if (!user) {
+        return res.status(404).json({message: "Body Empty"});
+    }
+    let accessToken = jwt.sign({
+        data: user
+      }, 'access', { expiresIn: 60 * 60 });
+
+      req.session.authorization = {
+        accessToken
+    }
+    return res.status(200).send("User successfully logged in");
+});
+
 const PORT =5000;
 
 app.use("/customer", customer_routes);
