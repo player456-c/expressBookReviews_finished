2fbd5897a1603b8556d055a10ee7f70fed2b7e4d
Matvey Bragin <veron.vbn1234@gmail.com>
2023-03-21 09:27:01 -0400

    First save

--- final_project/index.js
+++ final_project/index.js
@@ -12,13 +12,11 @@ app.use("/customer",session({
     secret:"fingerprint_customer",
     resave: true,
     saveUninitialized: true
-}));
+}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-
     if(req.session.authorization) {
-        let token = req.session.authorization['accessToken']; // Access Token
-        
+        token = req.session.authorization['accessToken'];
         jwt.verify(token, "access",(err,user)=>{
             if(!err){
                 req.user = user;
@@ -27,30 +25,15 @@ app.use("/customer/auth/*", function auth(req,res,next){
                 return res.status(403).json({message: "User not authenticated"})
             }
          });
-     } else {
+    }else{
          return res.status(403).json({message: "User not logged in"})
-     }
+    }
 });
 
-app.post("/login", (req,res) => {
-    const user = req.body.user;
-    //console.log(req);
-    if (!user) {
-        return res.status(404).json({message: "Body Empty"});
-    }
-    let accessToken = jwt.sign({
-        data: user
-      }, 'access', { expiresIn: 60 * 60 });
-
-      req.session.authorization = {
-        accessToken
-    }
-    return res.status(200).send("User successfully logged in");
-});
 
 const PORT =5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT,()=>console.log("Server is running on port "+PORT));
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,18 +5,64 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check if the username is valid
-}
+/* const doesExist = (username)=>{
+    let userswithsamename = users.filter((user)=>{
+      return user.username === username
+    });
+    if(userswithsamename.length > 0){
+        return true;
+    }else{
+        return false;
+    };
+}; */
 
-const authenticatedUser = (username,password)=>{ //returns boolean
+const isValid = (username)=>{
+//write code to check if the username is valid
+    let userswithsamename = users.filter((user)=>{
+        return user.username === username
+    });
+    if(userswithsamename.length > 0){
+        return true;
+    }else{
+        return false;
+    };
+};
+
+const authenticatedUser = (username,password)=>{
 //write code to check if username and password match the one we have in records.
+    let validusers = users.filter((user)=>{
+        return (user.username === username && user.password === password)
+    });
+    if(validusers.length > 0){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //console.log(req.body);
+
+    if (!username || !password) {
+        return res.status(404).json({message: "Error logging in"});
+    }
+
+    if (authenticatedUser(username,password)) {
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+
+        req.session.authorization = {
+            accessToken,username
+        }
+        return res.status(200).send("User successfully logged in");
+    }else{
+        return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }
 });
 
 // Add a book review
